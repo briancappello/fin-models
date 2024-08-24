44861bbd1553fcaffa991caaa7933bfe85087876
Brian Cappello <briancappello@gmail.com>
2024-08-24 12:26:52 -0600

    remove unused module

--- fin_models/analysis_utils.py
+++ fin_models/analysis_utils.py
@@ -113,6 +113,30 @@ def is_expanding_bodies(
     return is_advancing and bodies_expanding
 
 
+def is_crossed(
+    df: pd.DataFrame,
+    *,
+    column: str = None,
+    value: int | float = None,
+):
+    """
+    Returns true if there was an overnight or intraday cross of the given value
+    """
+    if column is None and value is None:
+        raise TypeError("One of `column` or `value` is required to be passed.")
+    elif column and column not in df.columns:
+        raise ValueError(f"The column {column!r} is missing from `df`.")
+
+    value = df[column].iloc[-1] if column else value
+    intraday_cross = df.iloc[-1].Open < value < df.iloc[-1].Close
+    overnight_cross = df.iloc[-2].Close < value < df.iloc[-1].Open
+    return intraday_cross or overnight_cross
+
+
+def num_bars_since_ts(df: pd.DataFrame, ts: pd.Timestamp):
+    return len(df) - df.index.get_loc(ts) - 1
+
+
 def median_volume(df: pd.DataFrame, num_bars: int = 50) -> float:
     if len(df) <= num_bars:
         return float(np.median(df.Volume))
