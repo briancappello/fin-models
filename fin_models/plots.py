bd4c0e5feaae3b7ed233c9b46ef557a8eef5e249
Brian Cappello <briancappello@gmail.com>
2024-10-27 11:41:56 -0600

    update plot_linear_regression to use deg=1

--- fin_models/plots.py
+++ fin_models/plots.py
@@ -7,16 +7,14 @@ import pandas as pd
 import fin_models.analysis_utils as au
 
 
-def plot_linear_regression(df: pd.DataFrame, num_bars: int = 50, deg: int = 1):
+def plot_linear_regression(df: pd.DataFrame, num_bars: int = 50):
     xs = list(range(num_bars))
     ys = df.iloc[-num_bars:]
-    trend = np.polyfit(xs, ys, deg)
+    slope, y_intercept = np.polyfit(xs, ys, deg=1)
     plt.scatter(xs, ys)
-    plt.plot(xs, np.polyval(trend, xs))
+    plt.plot(xs, np.polyval((slope, y_intercept), xs))
     plt.show()
-
-    if deg == 1:
-        return trend[0]  # trend == (slope, y_intercept); return slope
+    return slope
 
 
 def plot_local_min_max(df, nump=5):
