f0552e2a29cb711b63934e50cba4d3cc8fb79d87
Brian Cappello <briancappello@gmail.com>
2025-05-18 11:39:41 -0600

    parametrize fmt.sh script

--- fmt.sh
+++ fmt.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-poetry run ruff check --select I --fix  # isort
-poetry run ruff format  # black
+poetry run ruff check --select I --fix "$@" # isort
+poetry run ruff format "$@" # black
 
 # see the following issue for progress on a unified command that does both
 # https://github.com/astral-sh/ruff/issues/8232
