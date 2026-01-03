04d99777f2588f5111bdd46af486c9543ddcecd7
Brian Cappello <briancappello@gmail.com>
2026-01-03 07:52:20 -0700

    add Bar to top-level fin_models exports

--- fin_models/__init__.py
+++ fin_models/__init__.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from . import exchanges, indexes, vendors
 from .calendar import Calendar
+from .data_classes import Bar
 from .enums import Freq
 from .services import nyse, store
 from .store import Store
